488fe76ecfed4ec86db4b0a008196528acf0ca59
amirrezazahraei <120738467+amirrezazahraei@users.noreply.github.com>
2023-06-06 03:18:50 +0000

    done

---
+++ main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <limits>
+#include <string_view>
+
+void ignoreLine(){
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+}
+
+double getNumber(){
+
+    double number={};
+
+    while (true){
+    
+        std::cout<<"enter a number:"<<std::endl;
+        std::cin>>number;
+
+        if (!std::cin){
+            std::cin.clear();
+            ignoreLine();
+        }else{
+            ignoreLine();
+            return number;
+        }
+    }
+}
+
+char getOperatin(){
+
+    char operation = {};
+
+    while(true){
+        std::cout<<"enter an operation"<<std::endl;
+        std::cin>>operation;
+
+        if (!std::cin){
+            std::cin.clear();
+        }
+
+        switch (operation)
+        {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            return operation;
+        default:
+            std::cerr<<"the givven operation is not supported"<<std::endl;
+        }
+
+    }
+
+}
+
+void calculateResult(double number_1, char operation, double number_2){
+
+    switch (operation){
+        case '+':
+            std::cout<< number_1 + number_2;
+        case '-':
+            std::cout<< number_1 - number_2;
+        case '*':
+            std::cout<< number_1 * number_2;
+        case '/':
+            if (number_2 != 0.0)
+                std::cout<< number_1 / number_2;
+            std::cerr<<"sorry, can't devide by zero";
+    }
+}
+
+int main(){
+    double number_1 = {getNumber()};
+    double number_2 = {getNumber()};
+    char operation = {getOperatin()};
+    calculateResult(number_1, operation, number_2);
+    return 0;
+}
